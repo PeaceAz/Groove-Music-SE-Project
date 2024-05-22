dcbfbf36ba0f9a2871b4a0066dbaa15311bf2993
PeaceAz <Peaceazugo76@gmail.com>
2024-05-22 18:27:19 +0200

    Adding frontend code

--- controller/app.py
+++ controller/app.py
@@ -2,27 +2,26 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-app2 = Flask(__name__)
-# Correct the URI
-app2.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://Loving%4076@localhost/musicapp'
-db = SQLAlchemy(app2)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://Loving%4076@localhost/musicapp'
+db = SQLAlchemy(app)
 
 # Create model
-class Update_profile(db.Model):
-    id = db.Column(db.Integer, primary_key=True)  # Our columns
-    Username = db.Column(db.String(100), nullable=False)  # string max of 100
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+# class Update_profile(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)  # Our columns
+#     Username = db.Column(db.String(100), nullable=False)  # string max of 100
+#     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-    def __repr__(self):  # string representation of itself
-        return f"Username: {self.Username}"
+#     def __repr__(self):  # string representation of itself
+#         return f"Username: {self.Username}"
 
-    def __init__(self, Username):
-        self.Username = Username
+#     def __init__(self, Username):
+#         self.Username = Username
 
-@app2.route('/')
-def hello():
+@app.route('/')
+def root():
     return 'hey!'
 
 if __name__ == '__main__':
-    app2.run()
+    app.run()
 
