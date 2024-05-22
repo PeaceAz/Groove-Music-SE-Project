4838f5dadbc12bbba345c541fcc3c3de8c4cf898
PeaceAz <Peaceazugo76@gmail.com>
2024-05-22 11:58:19 +0200

    Getting rid of unneccesary files and also installing necessary packages

--- controller/app.py
+++ controller/app.py
@@ -1,28 +1,28 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import config
 from datetime import datetime
 
-app = Flask(__name__)
-app.config.from_object(config)
-db = SQLAlchemy(app)
- 
+app2 = Flask(__name__)
+# Correct the URI
+app2.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://Loving%4076@localhost/musicapp'
+db = SQLAlchemy(app2)
+
 # Create model
 class Update_profile(db.Model):
-    id = db.Column(db.Integer, primary_key=True)  #Our columns
-    Username = db.Column(db.String(100), nullable=False) #string max of 100
+    id = db.Column(db.Integer, primary_key=True)  # Our columns
+    Username = db.Column(db.String(100), nullable=False)  # string max of 100
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-    def __repr__(self):   # string representation of itself
-       return f"Username: {self.Username}" 
+    def __repr__(self):  # string representation of itself
+        return f"Username: {self.Username}"
 
     def __init__(self, Username):
-       self.Username = Username
+        self.Username = Username
 
-@app.route('/')
+@app2.route('/')
 def hello():
     return 'hey!'
 
 if __name__ == '__main__':
-    app.run()
+    app2.run()
 
