d3f823860b583cd38411dd7ddfeea515bb8912f1
PeaceAz <peaceazugo76@gmail.com>
2024-05-22 20:57:01 +0200

    refactored

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:Loving@76@localhost/musicapp')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- controller/app.py
+++ controller/app.py
@@ -1,25 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from controller.config import config
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://Loving%4076@localhost/musicapp'
+app.config.from_object(config)
 db = SQLAlchemy(app)
 
 # Create model
-# class Update_profile(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)  # Our columns
-#     Username = db.Column(db.String(100), nullable=False)  # string max of 100
-#     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+class Update_profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  #OUr columns
+    Username = db.Column(db.String(100), nullable=False) #string max of 100
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-#     def __repr__(self):  # string representation of itself
-#         return f"Username: {self.Username}"
+    def_repr__(self):   # string representation of itself
+       return f"Username: {self.Username}" 
 
-#     def __init__(self, Username):
-#         self.Username = Username
+    def__init__(self, Username):
+       self.Username = Username
 
 @app.route('/')
-def root():
+def hello():
     return 'hey!'
 
 if __name__ == '__main__':
